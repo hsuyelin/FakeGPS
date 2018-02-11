db2ee81f7e01a0a22ad2b61589f9daea55c85a22
徐艳玲 <hsuyelin@163.com>
2018-02-11 11:50:29 +0800

    project init

---
+++ FakeGPS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FakeGPS
+//
+//  Created by hsuyelin on 2018/2/11.
+//  Copyright © 2018年 adinnet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
